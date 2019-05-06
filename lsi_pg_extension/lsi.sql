8073a0227a08e717f2f19067057f869b77a0854a
jagadeesha <jagadeesha.kan@gmail.com>
2019-05-06 13:58:41 +0530

    simple_processing for text

--- lsi_pg_extension/lsi.sql
+++ lsi_pg_extension/lsi.sql
@@ -5,8 +5,11 @@ AS $$
     return "Model with %d topics and %d terms" % (lsi.num_topics, lsi.num_terms)
 $$ LANGUAGE plpython3u;
 
-CREATE OR REPLACE FUNCTION transform_to_model(words text[]) RETURNS real[]
+CREATE OR REPLACE FUNCTION transform_to_model(txt text[]) RETURNS real[]
 AS $$
+    from gensim.utils import smart_open, simple_preprocess
+    from gensim.parsing.preprocessing import STOPWORDS
+    words=[token for token in simple_preprocess(txt) if token not in STOPWORDS]
     lsi = GD['lsi_model']
     bow = lsi.id2word.doc2bow(words)
     return [v for k,v in lsi[bow]]
@@ -15,12 +18,15 @@ $$ LANGUAGE plpython3u;
 CREATE OR REPLACE FUNCTION transform_to_model(document text) RETURNS real[]
 AS $$
     import re
-    text = re.sub(r'(?:https?|ftp)://\S+', '', document)
+    from gensim.utils import smart_open, simple_preprocess
+    from gensim.parsing.preprocessing import STOPWORDS
+    txt = re.sub(r'(?:https?|ftp)://\S+', '', document)
     from gensim import utils
-    words = [token.encode('utf8') for token in
-        utils.tokenize(text, lower=True, errors='ignore')
-        if 2 <= len(token) <= 15 and not token.startswith('_')
-    ]
+    words=[token for token in simple_preprocess(txt) if token not in STOPWORDS]
+    #words = [token.encode('utf8') for token in
+    #    utils.tokenize(text, lower=True, errors='ignore')
+    #    if 2 <= len(token) <= 15 and not token.startswith('_')
+    #]
     
     lsi = GD['lsi_model']
     bow = lsi.id2word.doc2bow(words)
